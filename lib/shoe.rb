3a25a59fb74f9ae89d5210df8679affa92f66150
Vicki Hamm <vicki.cello@gmail.com>
2018-06-25 01:34:57 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,18 @@
-# Make your shoe class here!
+class Shoe
+  def initialize(brand)
+    @brand = brand
+  end
+  
+    def brand
+      @brand
+    end
+
+    def color=(color)
+      @color = color
+    end
+
+    def color
+      @color
+    end
+    
+end
